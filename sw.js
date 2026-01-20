30f1f33b002a015e364bfb544b00f118e4ca3e34
cube88 <flipcube88@gmail.com>
2026-01-20 19:56:22 +0800

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./index.html']))));self.addEventListener('fetch',e=>{if(!e.request.url.includes('api')){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))}});
